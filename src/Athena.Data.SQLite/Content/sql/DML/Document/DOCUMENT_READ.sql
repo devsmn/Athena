61a9195969d44a81a9c8322c2bba1db790222a67
devsmn <106095065+devsmn@users.noreply.github.com>
2025-04-22 14:02:52 +0200

    Load pinned status correctly

--- src/Athena.Data.SQLite/Content/sql/DML/Document/DOCUMENT_READ.sql
+++ src/Athena.Data.SQLite/Content/sql/DML/Document/DOCUMENT_READ.sql
@@ -3,6 +3,7 @@
 	   doc.DOC_comment as Comment,
 	   doc.DOC_thumbnail as ThumbnailString,
 	   doc.DOC_creationDate as CreationDate,
-	   doc.DOC_modDate as ModDate
+	   doc.DOC_modDate as ModDate,
+	   doc.DOC_isPinned as IsPinnedInteger
   FROM DOCUMENT doc
  WHERE CASE WHEN @DOC_ref = -1 THEN doc.DOC_ref = doc.DOC_ref ELSE doc.DOC_ref = @DOC_ref END;
