dfb019a130d6c9035d3a4464976a76e806fa278b
devsmn <106095065+devsmn@users.noreply.github.com>
2025-05-28 15:29:39 +0200

    Comments

--- src/Athena/Content/Service/Download/IDownloadService.cs
+++ src/Athena/Content/Service/Download/IDownloadService.cs
@@ -4,6 +4,14 @@ namespace Athena.UI
 {
     public interface IDownloadService : IDisposable
     {
+        /// <summary>
+        /// Asynchronously downloads the file at the given <paramref name="source"/> and saves
+        /// it at the provided <paramref name="localFilePath"/>.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="source"></param>
+        /// <param name="localFilePath"></param>
+        /// <returns></returns>
         Task DownloadAsync(IContext context, string source, string localFilePath);
     }
 }
--- src/Athena/Content/Service/Network/INetworkService.cs
+++ src/Athena/Content/Service/Network/INetworkService.cs
@@ -8,7 +8,16 @@ namespace Athena.UI
 {
     public interface INetworkService
     {
+        /// <summary>
+        /// Determines whether the device is connected to a WLAN network.
+        /// </summary>
+        /// <returns></returns>
         bool IsWifi();
+
+        /// <summary>
+        /// Determines whether internet access is possible.
+        /// </summary>
+        /// <returns></returns>
         bool IsInternetAccessPossible();
     }
 }
