cda53b8ec3c4f55867f12408d2ced067be831bf5
devsmn <106095065+devsmn@users.noreply.github.com>
2025-07-10 17:30:22 +0200

    ModuleInstaller is working

--- src/NativeLibraryInterop/moduleinstaller/native/moduleinstaller/src/main/java/com/spflaum/moduleinstaller/ModuleInstallerWrapper.java
+++ src/NativeLibraryInterop/moduleinstaller/native/moduleinstaller/src/main/java/com/spflaum/moduleinstaller/ModuleInstallerWrapper.java
@@ -7,13 +7,13 @@ import com.google.android.play.core.splitinstall.SplitInstallManager;
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory;
 import com.google.android.play.core.splitinstall.SplitInstallRequest;
 import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
-import com.google.android.play.core.splitinstall.SplitInstallSessionState;
 
-public class ModuleInstaller {
+public class ModuleInstallerWrapper {
 
     public interface InstallCallback {
         void onSuccess();
         void onFailure(Exception e);
+		void onProgress(int percent);
     }
 
     public static void installModule(Context context, String moduleName, InstallCallback callback) {
@@ -28,22 +28,32 @@ public class ModuleInstaller {
             .addModule(moduleName)
             .build();
 
-        SplitInstallStateUpdatedListener listener = state -> {
-            if (state.moduleNames().contains(moduleName)) {
-                switch (state.status()) {
-                    case SplitInstallSessionStatus.INSTALLED:
-                        callback.onSuccess();
-                        manager.unregisterListener(this);
-                        break;
-                    case SplitInstallSessionStatus.FAILED:
-                        callback.onFailure(new Exception("Module installation failed: " + state.errorCode()));
-                        manager.unregisterListener(this);
-                        break;
-                }
-            }
-        };
+        SplitInstallStateUpdatedListener[] listener = new SplitInstallStateUpdatedListener[1];
 
-        manager.registerListener(listener);
+		listener[0] = state -> {
+			if (state.moduleNames().contains(moduleName)) {
+				switch (state.status()) {
+					 case 2: // SplitInstallSessionStatus.DOWNLOADING:
+                        long totalBytes = state.totalBytesToDownload();
+						long downloadedBytes = state.bytesDownloaded();
+                        int percent = totalBytes > 0 ? (int)((downloadedBytes * 100L) / totalBytes) : 0;
+                        callback.onProgress(percent); 
+                        break;
+						
+					case 5: //SplitInstallSessionStatus.INSTALLED:
+						callback.onSuccess();
+						manager.unregisterListener(listener[0]); 
+						break;
+						
+					case 6: //SplitInstallSessionStatus.FAILED:
+						callback.onFailure(new Exception("Module installation failed: " + state.errorCode()));
+						manager.unregisterListener(listener[0]);
+						break;
+				}
+			}
+		};
+
+		manager.registerListener(listener[0]);
         manager.startInstall(request)
             .addOnFailureListener(callback::onFailure);
     }
